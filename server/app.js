04557d7b2dba93214c0a57e7640d4fb1022fd459
itsnotm3h <thewhit3pages@gmail.com>
2026-01-18 21:29:52 +0800

    refactor code

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors()); // Allows your React app (usually on port 3000 or 5173) to talk to this server
+app.use(express.json()); // Allow server to read json.
+
+app.get('/api/status', (req,res)=>{
+    res.json({message:"Backend is connected to React!"});
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
