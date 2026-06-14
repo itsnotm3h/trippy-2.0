13b43ac76a6502670e5ed9b660ea7ff2e08451dd
itsnotm3h <thewhit3pages@gmail.com>
2026-06-14 15:22:43 +0800

    updated migration schema

--- server/database/migration/20260614064749-create-users-table.js
+++ server/database/migration/20260614064749-create-users-table.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(` 
+    CREATE TABLE IF NOT EXIST users (
+    user_id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+    first_name VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+    last_name VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+    password VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci',
+    email VARCHAR(100) NOT NULL COLLATE 'latin1_swedish_ci',
+    display_name VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+    auth_id VARCHAR(50) NOT NULL DEFAULT '-' COLLATE 'latin1_swedish_ci',
+    invited_by INT(11) NULL DEFAULT NULL,
+    status ENUM('PENDING','ACTIVE','DEACTIVATED') NOT NULL DEFAULT 'ACTIVE' COLLATE 'latin1_swedish_ci',
+    PRIMARY KEY (user_id) USING BTREE,
+    UNIQUE INDEX email (email) USING BTREE
+  )
+`);
     /**
      * Add altering commands here.
      *
@@ -11,12 +27,15 @@ module.exports = {
      */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP TABLE IF EXISTS users
+      `);
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
